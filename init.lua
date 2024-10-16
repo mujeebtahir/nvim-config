c14a851c03b39a08f983ffc9f75e2cd4ea870e42
Mujeeb Tahir <mujeebvhr@gmail.com>
2024-10-17 01:36:34 +0500

    feat(autoformat, lspconfig): ensure installed clangd, markdownlint and enable lua autoformat

--- init.lua
+++ init.lua
@@ -146,4 +146,5 @@ require('bufferline').setup {}
 require 'custom.mappings'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
+--
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -201,6 +201,8 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'clangd',
+      'markdownlint',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
